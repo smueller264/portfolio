a91609b9c315f7234f0b7c960db074ce8a51fbbf
Sebastian Müller <sebastian@Sebastians-MBP.fritz.box>
2021-10-22 21:17:54 +0200

    inital commit

---
+++ lib/pages/bubblesort_page/bubblesort_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BubbleSortScreen extends StatelessWidget {
+  const BubbleSortScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Bubble Sort"),
+      ),
+    );
+  }
+}
